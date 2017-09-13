1882e50fe58cf70400d28a49cafb6aa24c8cb332
trevis <oss@silencedpoet.com>
2017-09-12 20:11:00 -0400

    Do the right stuff with poudriere

--- poudriere-rebuild-mail.sh
+++ poudriere-rebuild-mail.sh
@@ -11,14 +11,16 @@ MAILDOMAIN=silencedpoet.com
 /usr/local/bin/poudriere ports -u -p HEAD
 PORTS_CODE=$?
 
-if [ $POUDRIERE_CODE -ne 0 ]; then
+if [ $PORTS_CODE -ne 0 ]; then
   /usr/bin/mailx -s "Poudriere port update ${HOSTNAME} Failed!" "poudriere@${MAILDOMAIN}" "Failure"
 fi
 
 # Run the bulk update
-/usr/local/bin/poudriere ports bulk -j 11-1_release -p HEAD -f /usr/local/etc/poudriere.d/ports.poudriere
+/usr/local/bin/poudriere bulk -j 11-1_release -p HEAD -f /usr/local/etc/poudriere.d/ports.poudriere
 POUDRIERE_CODE=$?
 
 if [ $POUDRIERE_CODE -ne 0 ]; then
   /usr/bin/mailx -s "Poudriere bulk rebuild on ${HOSTNAME} Failed" "poudriere@${MAILDOMAIN}"  "Failure"
 fi
+
+chgrp -R www /usr/local/poudriere/data/logs/bulk/11-1_release-HEAD
