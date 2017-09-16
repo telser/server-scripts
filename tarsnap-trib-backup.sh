f331bb5a75bccc66de60d92f84224879a7da5f31
trevis <oss@silencedpoet.com>
2017-09-16 01:52:26 -0400

    Update trib path

--- tarsnap-trib-backup.sh
+++ tarsnap-trib-backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/tarsnap -c -f "$(uname -n)-$(date +%Y-%m-%d_%H-%M-%S)" --print-stats /etc /usr/pkg/etc
+/usr/pkg/bin/tarsnap -c -f "$(uname -n)-$(date +%Y-%m-%d_%H-%M-%S)" --print-stats /etc /usr/pkg/etc
